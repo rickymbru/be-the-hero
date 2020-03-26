a9f306b1e6785461a4b3b2b899199e8696b57abe
Ricky Menezes Bruggemann <ricky@cedae.com.br>
2020-03-26 13:25:15 -0300

    Back-end criado

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+const express = require('express'); // Pacote
+const cors = require('cors');
+const routes = require('./routes'); // Arquivo
+
+const app = express();
+
+app.use(cors(
+     {origin: '*'}
+));
+app.use(express.json());
+app.use(routes);
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP
+  * GET:    Buscar/Listar uma Informação do back-end
+  * POST:   Criar uma informação no back-end
+  * PUT    Alterar uma informação no back-end
+  * DELETE  Apagar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parâmetros:
+   * 
+   * Query Params: Parâmetros nomeados enviados na rota após "?"(filtros, paginação)
+   * Route Params: Parâmetros utilizados para identificar recurso
+   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+   */
+
+   /**
+    * SQL: MySQl, SQLite, PostgreSQl, Oracle, MSSQL
+    * No SQL: MOnogoDB, CouchDB, etc
+    */
+
+    /**
+     * Driver: SELECT * FROM users;
+     * Query Builder: table('users').select('*').where() KNEX.JS
+     */
+
+
+app.listen(3333);
+
