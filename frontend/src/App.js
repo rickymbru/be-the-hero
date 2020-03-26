76747198b2c24cb2abbb2956edd3fa0e71a91321
Ricky Menezes Bruggemann <ricky@cedae.com.br>
2020-03-26 19:40:55 -0300

    mudanca de ambiente

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Semana OmniStack
+          Semana OmniStack11
         </p>
         <a
           className="App-link"
